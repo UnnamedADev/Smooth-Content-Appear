46949d3ff59fb48d81842c9f7ee221e4d27bb77b
Unnamed Developer <unnamed.a.dev@gmail.com>
2018-01-30 01:33:41 +0100

    v0.0.2
    HTML
    + Created bigger example content
    JAVASCRIPT
    + Added code that binds event that starts checking all elements position and deciding which one should be visible now when user scrolls site
    CSS
    + Changed color of nav bar and footer
    + Added hover effect for menu elements

--- smooth.js
+++ smooth.js
@@ -1,7 +1,51 @@
 "use strict";
 
+//const
+    const duration = 300;
+    const widthDelay = 0;
+    const timeDelay = 200;
+
 $(document).ready(function(){
-    
+
     //code
+    var elmnts = $(document).find("p, h3, h4, h5, h6");
+    
+    for(var k=0;k<elmnts.length;k++){
+       elmnts[k].style.opacity = 0.0;
+    }
+    
+    check(elmnts);
+    
+    $(window).scroll(function(){
+        check(elmnts);
+    });
     
 });
+
+//functions
+
+function check(elmnts){
+
+    var usrStrt = $(window).scrollTop();
+    var usrEnd = usrStrt + $(window).height();
+    
+    for(var i=0;i<elmnts.length;i++){
+        
+        var elmntStart = $(elmnts[i]).position().top;
+        var elmntEnd = elmntStart + $(elmnts[i]).height();
+        
+       if(elmntEnd < usrEnd - widthDelay){
+            
+            if(elmnts[i].style.opacity == 0){
+                
+                    $(elmnts[i]).delay(timeDelay).animate({
+                            opacity: 1
+                        },duration);
+                    
+            } 
+       }
+        
+    }
+
+}
+
